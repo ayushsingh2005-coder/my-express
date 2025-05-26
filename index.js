78f99f7da702ca5daf0dd3036b4bc40eac3254d6
ayushsingh2005-coder <ayushsingh638684@gmail.com>
2025-05-26 06:06:03 +0530

    files added

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// main file 
+// main file
+const Path = require('path')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/about', (req, res) => {
+  res.sendFile(Path.join(__dirname,'index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
