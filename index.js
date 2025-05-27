0fd4b2d090335fd4542bbe63ef87f29a8108a88d
ayushsingh2005-coder <ayushsingh638684@gmail.com>
2025-05-27 18:05:25 +0530

    add more files

---
+++ index.js
@@ -0,0 +1,19 @@
+// INTRODUCTION :
+
+// in express or in http case ,api is all about settingup http interface to interact with our data(sent through json) and in order to send back our reslinse we're going to use res.json method which will do like example setting up the proper content type and stringfy our data.using RES,json method
+
+// SSR : this is the other side where we setup the templated and send back entire html,css ,js and we we'll do it by using res.render method()
+
+
+//  API - JSON  VS  SSR - TEMPLATE
+
+// SEND DATA         SEND TEMPLATE
+// RES.JSON()        RES.RENDER()
+
+
+
+// WHEN IT COMES TO EXPRESS IN MOST CASES , YOU'LL USE ONE OF  THE TWO FOLLOWING TWO OPTIONS :
+//🔸USE IT TO SETUP API
+//          OR
+//🔸TEMPLATES WITH  SSR (Server Side Rendering)
+
