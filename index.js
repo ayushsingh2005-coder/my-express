75d8a92dce387bab0d30e5ecd5ba754c1bf74e01
ayushsingh2005-coder <ayushsingh638684@gmail.com>
2025-06-27 18:30:24 +0530

    files added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 let {people} = require('./data');
 
 // static assest
-app.use(express.static('./'))
+app.use(express.static('./methods-public'));
 
 
 // first and default method used to read data 
@@ -13,9 +13,21 @@ app.get('/api/people' ,(req,res)=>{
     res.status(200).json({success : true , data : people});
 })
 
-
+// handling problem 1 stated below :
+app.post('/login',(req,res)=>{
+    res.send('POST');
+})
 
 
 app.listen(5000 , ()=>{
     console.log(`server is listening at http://localhost:5000`);
 })
+
+// remember when we are sending the get request we're not sending the body but when we're sending a post request it's very crucial . 
+
+// in index.html file ,the name attribute acts as key and the value inside it is the value of the key. 
+
+//PROBLEM THAT CAN ARISE DURING OPERATING WITH POST METHOD(when request is coming):
+
+// 1. We're not handling that
+// 2. We don't jave the middleware that actually adds this data that the form .
